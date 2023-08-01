027a1bf399c2cd41f55f128bf33bb6619d946fe9
Subarna Mainali <71552094+subarna200@users.noreply.github.com>
2023-08-01 19:21:43 +0545

    Update SendMail.ts

--- src/Service/SendMail.ts
+++ src/Service/SendMail.ts
@@ -4,6 +4,7 @@ export const sendEmail = async ({ email, subject, message }: any) => {
   const transporter: any = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
+      secure: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
